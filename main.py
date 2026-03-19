500b9fa4b04680ae5f3e5e6c993c1fc8cadc8c2b
Varun Phanindra Shrivathsa <varunpshrivathsa@gmail.com>
2026-03-19 13:57:55 -0500

    Add Open3D visualization for KITTI LiDAR scans

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from src.kitti_loader import KITTILoader
+from src.visualize import show_point_cloud
 
 def main():
     sequence_path = "/data/datasets/kitti/sequences/00"
     loader = KITTILoader(sequence_path)
 
-    print("Total Scans:",len(loader))
     points = loader.get_point_cloud(0)
-    print("First Scan Shape: ",points.shape)
-    print(points[:5])
-
+    show_point_cloud(points)
 
 
 
