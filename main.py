2685e81786e6d8c07f476120ce440a6a362babf2
Varun Phanindra Shrivathsa <varunpshrivathsa@gmail.com>
2026-03-19 13:52:28 -0500

    Add KITTI LiDAR loader and initial main test

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+from src.kitti_loader import KITTILoader
+
 def main():
-    print("LiDAR Odometry Pipeline")
+    sequence_path = "/data/datasets/kitti/sequences/00"
+    loader = KITTILoader(sequence_path)
+
+    print("Total Scans:",len(loader))
+    points = loader.get_point_cloud(0)
+    print("First Scan Shape: ",points.shape)
+    print(points[:5])
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
